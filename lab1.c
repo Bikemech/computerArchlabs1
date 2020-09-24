3c0ed355f5c32156f5c8cca104d1742b5428572f
Alexander Hoey <hoey.alexander@gmail.com>
2020-09-24 08:55:31 +0200

    fixed sign type detection

--- lab1.c
+++ lab1.c
@@ -2,19 +2,44 @@
 #include <stdlib.h>
 #include <cmath>
 
+void signtype()
+{
+	char a = 127;
+	char b = -127;
+	char r = a & b;	// Bitwise and operation
+	/*
+	*	a & b 	===		0111 1111 &
+	*	127 & -127		1111 1111 =
+	*					0111 1111 // for signed integers
+	*
+	*	a & b 	===		0111 1111 &
+	* 	127 & -127		1000 0001 =
+	*					0000 0001 // for two's complement
+	*/
+	
+	if (r == 1) printf("Using two's complement\n");
+	else printf("Using signed integer\n");
+}
+
+
 int main()
 {
-	char* mem = (char*) malloc(sizeof(int));
+	char* mem = (char*) malloc(sizeof(int) * 2);
 	int* ptr = (int*) mem;
+	*(ptr + 1) = 0; // Initializing to erase scrap data;
 
-	// *ptr = 0x04030201;
-	*ptr = (int)  pow(2, 31);
+	*(ptr + 1) = 0x04030201;
+	// *ptr = 0x80;
+
 
 	printf("\n");
 
-	for (int i = 0; i < sizeof(double); i++)
+	for (int i = 4; i < sizeof(int) + 4; i++)
 	{
-		printf("%x\t", (int) (255 + mem[i]) % 255);
+		printf("%x\t", (int) (256 + mem[i]) % 256);
 	}
 	printf("\n%d\n", *ptr);
+	signtype();
+
+
 }
--- quarterfloat.cpp
+++ quarterfloat.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 class qpfloat
 {
@@ -15,7 +16,7 @@ class qpfloat
 	// e-mach = 0.0625
 	// e-mach is represented as: 0 011 0001
 
-	// smallest non negative representable value = 1/(2^3 * 2^4) = 1/(127)
+	// smallest non negative representable value = 1/(2^3 * 2^4) = 1/(128)
 	// smallest num:	0 000 0001
 
 	// biggest representable value = 2^3 * (2 - 2^-4) = 15.5
@@ -24,27 +25,36 @@ class qpfloat
 
 	char data;
 public:
-	qpfloat(double i)
+	qpfloat(float i)
 	{
 		this->data = 0;
 		if (i > 15.5) this->data = 0x70;
 		if (i < -15.5) this->data = 0xf0;
+
+		char mem[sizeof(float)];
+		float* ptr = (float*) mem;
+		int* iptr = (int*) mem;
+		// *iptr = *iptr >> 3;
+		// *ptr = i;
+
+		printf("%d\n", *iptr);
+		for (int j = sizeof(float) - 1; j >= 0 ; j--)
+			printf("%x\t", (256 + mem[j]) % 256);
+
+		std::cout << std::endl;
 	}
 	~qpfloat();
 
 	std::string bin()
 	{
 
-		std::cout << (int) this->data << std::endl << std::endl;
-
-		char it = this->data;	// This is the char that i need for conversion to binary
-								// However using a signed char yields errors.
+		unsigned char it = this->data;	// This is the char that i need for conversion to binary
+										// However using a signed char yields errors.
 		char str[8];
 		int index = 7;
 
 		while (index + 1)
 		{
-			std::cout << (int) it << "\t"; // debugless debugger :>
 			if (it % 2)
 			{
 				str[index] = '1';
@@ -63,12 +73,14 @@ public:
 
 int main()
 {
-	qpfloat& t = *(new qpfloat(-20)); // less than minimum
-	qpfloat& s = *(new qpfloat(20)); // greater than maximum
+	qpfloat& t = *(new qpfloat(INFINITY)); // less than minimum
+	qpfloat& s = *(new qpfloat(-INFINITY)); // greater than maximum
 
 	
 	std::cout << t.bin() << std::endl;
 	std::cout << s.bin() << std::endl;
+
+
 
 	return 0;
 }
